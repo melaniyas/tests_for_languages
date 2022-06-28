350b3aaaa23de807d2c534957275199ce841c8ea
melaniascastlivceva <melaniya2001@mail.com>
2022-06-28 15:41:42 +0300

    доработка кода теста

---
+++ test_items.py
@@ -0,0 +1,10 @@
+import time
+from selenium.webdriver.common.by import By
+
+
+def test_buscket(browser):
+    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+)
+    time.sleep(5)
+    add_to_basket = browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
+    assert add_to_basket, 'кнопка добавления товара отсутвует'
