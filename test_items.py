cc78fb305242427376fe2ec5b1a4a98414695213
melaniyas <103375034+melaniyas@users.noreply.github.com>
2023-03-21 14:23:09 +0200

    update test

--- test_items.py
+++ test_items.py
@@ -2,8 +2,7 @@ import time
 from selenium.webdriver.common.by import By
 
 
-def test_button_basket(browser):
-    browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+def test_btn_add_to_basket(browser):
+    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
     time.sleep(5)
-    add_to_basket = browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
-    assert add_to_basket, 'кнопка добавления товара отсутвует'
+    assert browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket"), "button not found"
